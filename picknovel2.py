e943139482ae7cc3fcaab594810d059ba031428f
mjqgh <mjqtailscale@outlook.com>
2025-10-21 12:00:10 +0800

    Add comment to pn_book_info function
    Add a comment to the pn_book_info function to clarify its purpose.

--- picknovel2.py
+++ picknovel2.py
@@ -2,6 +2,7 @@ import requests
 
 
 def pn_book_info(cookie, pn_id):
+    # 获取pn小说的书籍信息
     url = f"http://aikan-admin.thnovel.com/Book/getList?page=1&limit=20&book_source=-1&keyword={pn_id}&status=-1&h_status=-1&sign_type=-1&cost_type=-1&cate_id=-1&is_tweet=-1&type=&update_date=&lang=en"
     headers = {
         "Cookie": cookie,
