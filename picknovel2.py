6b1d97a59db1d7eb50652bfe4dd5b0c409641e35
mjqgh <mjqtailscale@outlook.com>
2025-10-21 11:57:15 +0800

    Comment out total items check in response
    Comment out the check for total items in response.

--- picknovel2.py
+++ picknovel2.py
@@ -8,8 +8,8 @@ def pn_book_info(cookie, pn_id):
         "X-Requested-With": "XMLHttpRequest",
     }
     rsp = requests.get(url, headers=headers).json()
-    if rsp["data"]["total"] == 0:
-        return None
+    # if rsp["data"]["total"] == 0:
+    #     return None
     dict_rsp = rsp["data"]["items"][0]
     # init_charge_section 卡点锁章
     return dict_rsp
