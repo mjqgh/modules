775f270653e7e6f361da760912d9ec12f475174c
mjqgh <mjqtailscale@outlook.com>
2025-11-13 19:44:22 +0800

    Refactor h5_edit_book_status to include status parameter
    Updated h5_edit_book_status function to accept status and fetch cookies from a JSON URL.

--- master.py
+++ master.py
@@ -21,16 +21,25 @@ def h5_book_info(hn_id):
     # h5的小说id：id
     return df_info
 
-def h5_edit_book_status(pid):
-    # 批量审核小说上架
-    edit_api = "http://aikan-admin.thnovel.com/Book/editBatchStatus"
+def h5_edit_book_status(h5_id, status=1):
+    # 批量审核小说上架/下架
+    # status=1为上架、2为下架、3为软下架，默认为1
+    url_cookie = "https://raw.githubusercontent.com/mjqgh/modules/refs/heads/main/cookies.json"  # 从在线json中获取cookie
+    dict_cookies = requests.get(url_cookie).json()
+    cookie_h5 = dict_cookies["h5"]
+    
+    edit_api = "http://master-admin.thnovel.com/Book/editBatchStatus"
     post_data = {
         "lang": "en",
-        "status": 1,
-        "product_list": json.dumps([{"productId":f"{pid}"},{"productId":""}]),
+        "status": status,  # 默认上架
+        "product_list": json.dumps([{"productId":f"{h5_id}"},{"productId":""}]),
         "is_sync_status": 1
     }
-    response = requests.post(edit_api, headers=self.headers, data=post_data).text
+    headers = {
+        "Cookie": cookie_h5,
+        "X-Requested-With": "XMLHttpRequest"
+    }
+    response = requests.post(edit_api, headers=headers, data=post_data).text
     return response
 
 def h5_edit_book_price(h5_id, kadian, price_type=1, price=40):
