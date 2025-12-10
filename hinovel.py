48fca6ccc5ea6dbe2912763ecfe58b93d9578cab
mjqgh <mjqtailscale@outlook.com>
2025-12-10 11:53:14 +0800

    Fix authorization header format in hinovel.py

--- hinovel.py
+++ hinovel.py
@@ -23,7 +23,7 @@ def each_day_adspend(start_date, end_date, access_token):
         "X-ads-aid": "hinovel",
         "X-ads-ip": "8.219.67.223",
         "X-ads-timestamp": f"{int(time.time())}",
-        "Authorization": f"Bearer {access_token}",
+        "Authorization": access_token,
     }
     rsp = requests.post(url=base_url, data=post_data, headers=headers).json()
     df = pd.DataFrame(rsp['data'])
